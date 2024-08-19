7ea31e59dfd93be24d54b3ab97f7f65dd486d7ff
Daniel <siddheshbhadale@gmail.com>
2024-08-19 17:02:11 +0530

    UPDATE: New Files Added

--- App.jsx
+++ App.jsx
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Auth from './src/auth/Auth'
+import Home from './src/screens/Home'
+import Stats from './src/screens/Stats'
+import Menu from './src/screens/Menu'
+import BottomNavigation from './src/navigation/BottomNavigation'
 
 const App = () => {
+
+  const Stack = createNativeStackNavigator()
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="BottomNavigation" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Auth" component={Auth} />
+        <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Stats" component={Stats} />
+        <Stack.Screen name="Menu" component={Menu} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
