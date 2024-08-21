fbc9b8d7c81c297a006f6db8f39c23ac0e5eda4f
Victorsward32 <sumitjadhav6067@gmail.com>
2024-08-21 11:37:47 +0530

    merge

--- App.jsx
+++ App.jsx
@@ -7,16 +7,9 @@ import Home from './src/screens/Home'
 import Stats from './src/screens/Stats'
 import Menu from './src/screens/Menu'
 import BottomNavigation from './src/navigation/BottomNavigation'
-<<<<<<< HEAD
 import LoginScreen from './src/auth/LoginScreen'
 import RegistrationScreen from './src/auth/RegistrationScreen'
 import ForgetPassword from './src/auth/ForgetPassword'
-=======
-import Login from './src/auth/Login'
-import Register from './src/auth/Register'
-import ForgotPasswordScreen from './src/auth/ForgotPasswordScreen'
-import ChangePassword from './src/auth/ChangePassword'
->>>>>>> 5a279dcf79335e22359512d064d9ada4003a8a35
 
 const App = () => {
 
@@ -24,26 +17,15 @@ const App = () => {
 
   return (
     <NavigationContainer>
-<<<<<<< HEAD
       <Stack.Navigator initialRouteName="RegistrationScreen" screenOptions={{ headerShown: false }}>
-=======
-      <Stack.Navigator initialRouteName="Auth" screenOptions={{ headerShown: false }}>
->>>>>>> 5a279dcf79335e22359512d064d9ada4003a8a35
         <Stack.Screen name="Auth" component={Auth} />
         <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Stats" component={Stats} />
         <Stack.Screen name="Menu" component={Menu} />
-<<<<<<< HEAD
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} />
         <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
-=======
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} />
-        <Stack.Screen name="ChangePassword" component={ChangePassword} />
->>>>>>> 5a279dcf79335e22359512d064d9ada4003a8a35
       </Stack.Navigator>
     </NavigationContainer>
   )
